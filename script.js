8f325f74e7f0a52ede448c186cfb1c8ea9aaf4ca
marsila <eng.rasha.ali7@gmail.com>
2025-10-13 13:48:24 +0200

    Negative number button

--- script.js
+++ script.js
@@ -8,18 +8,25 @@ const calcOperators = document.querySelector(".calc-operators");
 console.log(calcOperators);
 
 calcNumbers.addEventListener('click',(e)=> {
-    if(e.target.className === "calc-item"){
-        
+    if(e.target.className === "calc-item"){        
         output.value += e.target.textContent;
+        console.log(`id = ${e.target.id} value = ${e.target.textContent}`);
         if (e.target.id ==='clear-all'){
             output.value = '';
-            console.log(`output = ${output.value}`);
-            
+            console.log(`output = ${output.value}`);            
         } 
+
         if(e.target.id === 'clear-last'){
             output.value = output.value.slice(0,output.value.length-2);
         }
-        console.log(`id = ${e.target.id} value = ${e.target.textContent}`);
+
+        if(e.target.id === 'negative'){
+            const newOutput =output.value.slice(0,output.value.length-3);
+            if(newOutput[0]==='-'){
+                output.value = newOutput.slice(1, newOutput.length);
+            }else {output.value = `-${newOutput}`;}
+        }
+        
     }
 })
 
